8a60bb920a9843400a909f2a50a13c0682dee66a
Rohit Gupta <64639204+rohitgupta29@users.noreply.github.com>
2025-05-23 20:06:39 +0530

    Create longest_common_subsequence.py

---
+++ dynamic_programming/longest_common_subsequence.py
@@ -0,0 +1,30 @@
+
+
+def longestCommonSubsequence(text1, text2):
+    m, n = len(text1), len(text2)
+    # Create DP table
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    # Fill the DP table
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Backtrack to find the LCS string
+    i, j = m, n
+    lcs = []
+    while i > 0 and j > 0:
+        if text1[i - 1] == text2[j - 1]:
+            lcs.append(text1[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    # The lcs list contains the LCS in reverse order
+    return ''.join(reversed(lcs))
